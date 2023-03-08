c9606fdf7ee3eb2706ace9c5126ab9295538b988
Aneta <anette812s@gmail.com>
2023-03-08 16:51:45 +0000

    hmm refactor

--- hmm.py
+++ hmm.py
@@ -12,36 +12,29 @@ SMOOTHING = {
 
 class HiddenMarkovModel:
 
-    DECODING = ["global", "local"]
-
-    def __init__(self, A: np.array = None, B: np.array = None, state_map: dict = None) -> None:
+    def __init__(self, A: np.array = None, B: np.array = None, pi: np.array = None, state_map: dict = None) -> None:
         """_summary_
 
         Args:
             A (np.array, optional): Transition probability matrix. Defaults to None.
             B (np.array, optional): Emission probability matrix. Defaults to None.
         """
-        self.A = A
-        self.B = B
+        self.A = A  # transitions
+        self.B = B  # emissions
+        self.pi = pi  # initial states
         self._state_map = state_map
 
     # fit
-    # TODO make invariant to the number of passed features!!!!
+    # TODO extend to n-grams??
     def estimate_parameters(self,
                             X: list[Tuple[str]],  # emissions
                             Y: list[list[str]],  # transitions
                             smoothing: nltk.ProbDistI = nltk.WittenBellProbDist,
                             kwargs: dict = {"bins": 1e5}) -> None:
-        self.A = self._get_transitions(Y, smoothing, kwargs)
+        self.A, self.pi = self._get_transitions(Y, smoothing, kwargs)
         self.B = self._get_emissions(X, smoothing, kwargs)
 
-    def decode(self, decoding):
-        # Find most likely state (local) or sequence of states (global) given a sequence of observations
-        if decoding.lower() not in self.DECODING:
-            raise NotImplementedError(
-                "Unknown decoding type. Choose from ['global', 'local']")
-
-    def viterbi(self, samples: list[list[str]]):
+    def predict_viterbi(self, samples: list[list[str]]):
         predictions = []
         best_path_probabilities = []
         for s in samples:
@@ -52,26 +45,25 @@ class HiddenMarkovModel:
 
     def _viterbi(self, observations: list[str]):  # -> Tuple(list[str], float):
         # method to find a global decoding
-        # TODO sort out shapes of A and B
-        N = len(self.A)
-        T = len(observations)
+
+        N, T = len(self.A), len(observations)
+        # Viterbi and backpointers matrices
         V, B = np.zeros(shape=[N, T]), np.zeros(shape=[N, T], dtype=int)
 
-        # initialize
-        # state == 0 is <S> and state == N is </S>
-        for s in range(1, N-1):  # 1-17
-            V[s, 0] = np.exp(np.log(self.A[0, s]) +
+        # initialize (T=0)
+        for s in range(N):
+            V[s, 0] = np.exp(np.log(self.pi[s]) +
                              np.log(self.B[s].prob(observations[0])))
             B[s, 0] = 0
+        # populate
         for t in range(1, T):
-            for s in range(1, N-1):
+            for s in range(N):
                 # probabilities for each state given the path
-                probs = np.array([0] + [np.exp(np.log(V[s_, t-1]) + np.log(self.A[s_, s]))
-                                 for s_ in range(1, N-1)] + [0])
+                probs = np.exp(np.log(V[:, t-1]) + np.log(self.A[:, s]))
                 V[s, t] = np.exp(np.log(self.B[s].prob(
-                    observations[t])) + np.max(np.log(probs)))
+                    observations[t])) + np.log(np.max(probs)))
                 B[s, t] = np.argmax(probs)
-
+        # terminate
         best_path_probability = np.max(V[:, T-1])
         best_path_pointer = np.argmax(V[:, T-1])
         best_path = [best_path_pointer]
@@ -82,27 +74,30 @@ class HiddenMarkovModel:
         return prediction, best_path_probability
 
     def _get_transitions(self, Y: list[list[str]], smoothing: nltk.ProbDistI, kwargs: dict) -> np.array:
-        pos_tags = list(set([i for sen in Y for i in sen]))
+        if not self._state_map:
+            self._state_map = {i: v for i, v in enumerate(
+                list(set([i for sen in Y for i in sen])))}
+
+        pos_tags = list(self._state_map.values())
 
         # Get occureces counts across the sample
         pos_frequencies = {i: nltk.FreqDist()
-                           for i in pos_tags + ["<S>", "</S>"]}
-
+                           for i in pos_tags}
+        starting_frequencies = nltk.FreqDist()
         for sentence in Y:
             # get start of sentence transitions
-            pos_frequencies["<S>"][sentence[0]] += 1
-            # get end of sentence transitions
-            pos_frequencies[sentence[-1]]["</S>"] += 1
+            starting_frequencies[sentence[0]] += 1
             # get other transitions
-            for i in range(1, len(sentence)-1):
+            for i in range(len(sentence)-1):
                 pos_frequencies[sentence[i]][sentence[i+1]] += 1
 
         # Normalize to get probabilities
         A = {i: smoothing(pos_frequencies[i], **kwargs)
-             for i in pos_tags + ["<S>", "</S>"]}
+             for i in pos_tags}
+        pi = smoothing(starting_frequencies, **kwargs)
+        pi_array = np.array([pi.prob(i) for i in pos_tags])
 
-        # return A
-        return self._dist_dict_to_array(A)
+        return self._dist_to_array(A), pi_array
 
     def _get_emissions(self, X: list[Tuple[str]], smoothing: nltk.ProbDistI, kwargs: dict) -> list[nltk.ProbDistI]:
 
@@ -112,89 +107,15 @@ class HiddenMarkovModel:
 
         B = [0] * len(self._state_map)
         for i, v in self._state_map.items():
-            if v not in ["<S>", "</S>"]:
-                B[i] = B_dict[v]
+            B[i] = B_dict[v]
 
         return B
 
-    def _dist_dict_to_array(self, dist_dict: dict) -> np.array:
+    def _dist_to_array(self, dist_dict: dict) -> np.array:
         N = len(dist_dict.keys())
         arr = np.zeros([N, N])
-        if not self._state_map:
-            # self._state_map = {k: i+1 for i, k in enumerate(
-            #     [k for k in dist_dict.keys() if k not in ["<S>", "</S>"]])}
-            # self._state_map["<S>"] = 0
-            # self._state_map["</S>"] = N-1
-            self._state_map = {i+1: k for i, k in enumerate(
-                [k for k in dist_dict.keys() if k not in ["<S>", "</S>"]])}
-            self._state_map[0] = "<S>"
-            self._state_map[N-1] = "</S>"
 
         for i, v in self._state_map.items():
             for j, v_ in self._state_map.items():
                 arr[i][j] = dist_dict[v].prob(v_)
         return arr
-
-
-def prune_sentence(sent):
-    return [token for token in sent if type(token['id']) is int]
-
-
-def conllu_corpus(path):
-    data_file = open(path, 'r', encoding='utf-8')
-    sents = list(parse_incr(data_file))
-    return [prune_sentence(sent) for sent in sents]
-
-
-if __name__ == "__main__":
-    pass
-    # from conllu import parse_incr
-    # train_corpus_path = 'treebanks/UD_English-GUM/en_gum-ud-train.conllu'
-    # train_sents = conllu_corpus(train_corpus_path)
-
-    # m = HiddenMarkovModel()
-    # d = m.estimate_parameters(
-    #     train_sents, [[i['upos'] for i in s] for s in train_sents])
-    # m.viterbi(["Don't", "lose", "focus"])
-
-    # A_counts = self._count_occurences(Y)
-    # pos_tags = [i for i in A_counts.keys() if i not in ["<S>", "<\S>"]]
-
-    # # normalize to get probabilities
-    # A = { i: { j :  A_counts[i][j]/A_counts[i]["TOTAL"] \
-    #                 for j in pos_tags} \
-    #                 for i in A_counts.keys()}
-    # return A
-
-    # # check if we have empty categories
-    # removed = []
-    # for k in A_counts.keys():
-    #     if A_counts[k]["TOTAL"] == 0:
-    #         # remove rows
-    #         A_counts.pop(k)
-    #         removed.append(k)
-    # # remove columnd
-    # if removed:
-    #     for k in A_counts.keys():
-    #         for r in removed:
-    #             A_counts[k].pop(r)
-
-    # def _count_occurences(self, Y: list[list[str]]) -> dict:
-    #     pos_tags = list(set([word for sen in Y for word in sen]))
-
-    #     A_counts = { i: { j : 0  for j in pos_tags + ["TOTAL"] } \
-    #                              for i in pos_tags + ["<S>", "<\S>"]}
-
-    #     A_counts["<S>"]["TOTAL"], A_counts["<\S>"]["TOTAL"]  = len(Y), len(Y)
-
-    #     for sentence in Y:
-    #         # get start of sentence transitions
-    #         A_counts["<S>"][sentence[0]] += 1
-    #         # get end of sentence transitions
-    #         A_counts["<\S>"][sentence[-1]] += 1
-    #         # get other transitions
-    #         for i in range(1,len(sentence)-1):
-    #             A_counts[sentence[i]][sentence[i+1]] += 1
-    #             A_counts[sentence[i]]["TOTAL"] += 1
-
-    #     return A_counts
